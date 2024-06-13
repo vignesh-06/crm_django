ccdf0d0742a829d9095d32338f14ba8a20dee5d4
Vignesh V <84457406+vignesh-06@users.noreply.github.com>
2024-06-13 14:41:28 +0530

    Update models.py

--- website/models.py
+++ website/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from website.validators import validate_custom_email
 
 
 class Record(models.Model):
 	created_at = models.DateTimeField(auto_now_add=True)
 	first_name = models.CharField(max_length=50)
 	last_name =  models.CharField(max_length=50)
-	email =  models.CharField(max_length=100)
-	phone = models.CharField(max_length=15)
+	email =  models.CharField(max_length=100,unique=True, validators=[validate_custom_email])
+	phone = models.CharField(max_length=16, blank=True, null=True)
 	address =  models.CharField(max_length=100)
 	city =  models.CharField(max_length=50)
 	state =  models.CharField(max_length=50)
